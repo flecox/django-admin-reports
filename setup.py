fe5ac87819ce4c6ba65320734ec9f688613950b1
flecox <flecox@gmail.com>
2015-10-02 11:55:36 -0300

    package files

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-admin-reports',
+    version='0.1',
+    packages=['admin_reports'],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A simple Django app that allows downloading a csv file of the list view of a model',
+    long_description=README,
+    url='http://www.example.com/',
+    author='Gonzalo Almeida',
+    author_email='flecox@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
